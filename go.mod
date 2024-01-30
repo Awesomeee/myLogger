24ca23df93af62c4e3bc395ac0b98f6e78dd4424
HPTVIETNAM\duyl <duyle.computer@gmail.com>
2024-01-30 10:18:40 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Awesomeee/myLogger
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,17 @@
+package myLogger
+
+import (
+	"log"
+)
+
+func logInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func logWarning(message string) {
+	log.Printf("WARN - %v", message)
+}
+
+func logError(message string) {
+	log.Printf("ERROR - %v", message)
+}
